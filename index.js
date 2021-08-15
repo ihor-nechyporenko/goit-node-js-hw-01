95dc8c2c221f77caea4ad2be8391a54719b7b58e
Ihor Nechyporenko <nechyporenko.i@gmail.com>
2021-08-15 20:40:44 +0300

    Code refactoring 2.0

--- index.js
+++ index.js
@@ -4,51 +4,45 @@ const contactsOperations = require('./contacts');
 // const contactsOperations = require('./db');
 
 program
-  .option('-a, --action <type>', 'choose action')
-  .option('-i, --id <type>', 'user id')
-  .option('-n, --name <type>', 'user name')
-  .option('-e, --email <type>', 'user email')
-  .option('-p, --phone <type>', 'user phone');
+    .option('-a, --action <type>', 'choose action')
+    .option('-i, --id <type>', 'user id')
+    .option('-n, --name <type>', 'user name')
+    .option('-e, --email <type>', 'user email')
+    .option('-p, --phone <type>', 'user phone');
 
 program.parse(process.argv);
 
 const argv = program.opts();
 
 function invokeAction({ action, id, name, email, phone }) {
-  switch (action) {
-      case 'list':
-          (async () => {
-              await contactsOperations.listContacts();
-          })();
-      break;
+    switch (action) {
+        case 'list':
+            contactsOperations.listContacts();
+            break;
 
-      case 'get':
-          (async () => {
-              id
-                  ? await contactsOperations.getContactById(id)
-                  : console.log('ID is not specified');
-          })();
-      break;
+        case 'get':
+            id
+                ? contactsOperations.getContactById(id)
+                : console.log('ID is not specified');
+            break;
 
-      case 'add':
-          (async () => {
-              name && email && phone
-                  ? await contactsOperations.addContact(name, email, phone)
-                  : console.log('Please, entry full data (name, email, phone)');
-          })();
-      break;
+        case 'add':
+ 
+                name && email && phone
+                    ? contactsOperations.addContact(name, email, phone)
+                    : console.log('Please, entry full data (name, email, phone)');
+          
+            break;
 
-      case 'remove':
-          (async () => {
-              id
-                  ? await contactsOperations.removeContact(id)
-                  : console.log('ID is not specified');
-        })();
-      break;
+        case 'remove':
+                id
+                    ? contactsOperations.removeContact(id)
+                    : console.log('ID is not specified');
+            break;
 
-    default:
-      console.warn('\x1B[31m Unknown action type!');
-  }
+        default:
+            console.warn('\x1B[31m Unknown action type!');
+    }
 }
 
 invokeAction(argv);
